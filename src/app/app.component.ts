fcb01d83daa1183fa5f378f6d022fcd0cfca6abc
abhisek102 <abhisek.nanda@contractor.tatacommunications.com>
2022-05-30 14:47:37 +0530

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,14 @@ import { Component, VERSION } from '@angular/core';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
+export class AppComponent {
   name = 'Angular ' + VERSION.major;
+  arr = [
+    { name: 'johnny sins', email: 'johnny.bhai@gmail.com', num: 9008739291 },
+    { name: 'Danny D', email: 'Danny69@gmail.com', num: 7005486591 },
+    { name: 'Hardick pandiya', email: 'pandiya@gmail.com', num: 5464564654 },
+    { name: 'Dani Daniels', email: 'AkaDani@gmail.com', num: 9005486955 },
+  ];
 }
